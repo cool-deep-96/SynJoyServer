e721533dd3297a80d165758042ab384d61efd5f0
cool-deep-96 <27581kuldeep.2021cse@gmail.com>
2024-09-13 11:53:06 +0530

    cert

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ import { socketServer } from './sockets.js';
 import { roomRoutes } from './routes/roomRoutes.js';
 import fs from 'fs';
 import https from 'https';
+import path from 'path';
 
-
+const __dir = path.resolve()
+const sslAuthFile = path.join(__dir, '/5DA733F0C07C144D802078B0FC9DEE0C.txt')
 // const key = fs.readFileSync('private.key');
 // const cert = fs.readFileSync('certificate.crt');
 
@@ -43,6 +45,9 @@ const io = new Server(httpsServer, {
 
 socketServer(io);
 
+app.use('/.well-known/pki-validation/5DA733F0C07C144D802078B0FC9DEE0C.txt', (req, res) => {
+    res.sendFile(sslAuthFile);
+})
 
 app.use('/api/roomRoutes', roomRoutes);
 
